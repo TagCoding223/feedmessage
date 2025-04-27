a0281eb72d76af01902b0c6847db8875f6610412
TagCoding223 <vishalchouhanv.c22.3@gmail.com>
2025-04-27 15:48:39 +0530

    Delete Message Api Route

---
+++ src/app/api/delete-message/[messageId]/route.ts
@@ -0,0 +1,44 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+import { getServerSession, User } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+
+export async function DELETE(request:Request, {params}: {params:{messageId:string}}){
+    const messageId = params.messageId
+
+    await dbConnect()
+    const session = await getServerSession(authOptions)
+    const user: User = session?.user as User
+
+    if(!session || !session.user){
+        return Response.json({
+            success: false,
+            message: "Not Authenticated"
+        },{status: 401})
+    }
+
+    try {
+        const updatedResult = await UserModel.updateOne(
+            {_id: user._id},
+            {$pull: {messages: {_id: messageId}}}
+        )
+
+        if (updatedResult.modifiedCount==0) {
+            return Response.json({
+                success: false,
+                message: "Message not found or already deleted."
+            },{status: 404})
+        }
+
+        return Response.json({
+            success: true,
+            message: "Message Deleted."
+        },{status: 404})
+    } catch (error) {
+        console.error("Error occur in delete a message: ",error);
+        return Response.json({
+            success: false,
+            message: `Error deleting message: ${error}`
+        },{status: 500})
+    }
+}
